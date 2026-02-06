276f332103ccad6abecae240855ab86dbf259156
rapiddeveloper <77062058+rapiddeveloper@users.noreply.github.com>
2026-02-06 05:49:15 +0100

    Initial Commit

---
+++ DevCareer/DevCareerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevCareerApp.swift
+//  DevCareer
+//
+//  Created by Orisajobi Akinbola on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct DevCareerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
