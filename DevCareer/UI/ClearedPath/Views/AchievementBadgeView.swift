767fe9e748892c0bab6ba64da1219f9db6ad6bdb
rapiddeveloper <77062058+rapiddeveloper@users.noreply.github.com>
2026-02-06 17:02:10 +0100

    added animation flip to badge

--- DevCareer/UI/ClearedPath/Views/AchievementBadgeView.swift
+++ DevCareer/UI/ClearedPath/Views/AchievementBadgeView.swift
@@ -8,18 +8,40 @@
 import SwiftUI
 
 struct AchievementBadgeView: View {
+
+    @Binding var isFlipped: Bool
+
     var body: some View {
         ZStack {
             BadgeDecoration()
-            
+
                 .clipShape(.rect)
                 .frame(width: 331, height: 218)
+            ZStack {
+                if !isFlipped {
+                    badgeView
+                } else {
+                    badgeView
  
-            Image("BadgePurple")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 243, height: 213)
-              
+                 }
+            }
+            .rotation3DEffect(
+                .degrees(isFlipped ? 180 : 0),
+                axis: (x: 0.0, y: 0.0, z: 1.0),
+                perspective: 0.5
+            )
+            .animation(
+                .spring(response: 0.6, dampingFraction: 0.7), value: isFlipped)
+
         }
-     }
+    }
+}
+
+extension AchievementBadgeView {
+    var badgeView : some View {
+        Image("BadgePurple")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 243, height: 213)
+    }
 }
--- DevCareer/UI/ClearedPath/Views/ClearedPathView.swift
+++ DevCareer/UI/ClearedPath/Views/ClearedPathView.swift
@@ -10,20 +10,22 @@ import SwiftUI
 struct ClearedPathView: View {
     
     @Environment(ThemeStore.self) private var themeStore
+    @Environment(\.displayScale) var displayScale
+    @State private var isFlipped = false
     
     var body: some View {
         VStack(spacing: themeStore.xlSpacing) {
 
             // Header Action
             FlipBadgeButton {
-                print("Flipping...")
-            }
+                isFlipped.toggle()
+             }
  
            // Spacer()
 
             // Content Container
             VStack(spacing: themeStore.xlSpacing) {
-                AchievementBadgeView()
+                AchievementBadgeView(isFlipped: $isFlipped)
 
                 // Text Content
                 VStack(spacing: themeStore.mdSpacing) {
@@ -45,14 +47,40 @@ struct ClearedPathView: View {
             //Spacer()
 
             // Footer Action
-            DevCareerButton("Share your achievement", variant: .full) {
-
-            }
+            /*
+            ShareLink(
+                            item: renderBadge(),
+                            preview: SharePreview("My Mastery Badge", image: renderBadge())
+                        ) {
+                            Label("Share your achievement", systemImage: "photo")
+                                .padding()
+                                .background(Color.purple)
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                        }*/
+//            DevCareerButton("Share your achievement", variant: .full) {
+//
+//            }
         }
         .background(Color.white)
         .padding(.horizontal)
         .padding(.vertical, 36)
      }
+    
+    /*
+        @MainActor
+        private func renderBadge() -> Image {
+            let renderer = ImageRenderer(content: badgeView)
+            
+            // Ensure high resolution for sharing
+            renderer.scale = displayScale
+            
+            if let uiImage = renderer.uiImage {
+                return Image(uiImage: uiImage)
+            }
+            
+            return Image(systemName: "xmark.circle")
+        }*/
 }
 
 #Preview {
